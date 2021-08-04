78a933fb4532dc126e7fd9c9eb4218d37f522ef2
Nicholas Stevens <nstevens@nanick.org>
2021-08-04 18:30:05 -0500

    Update Encode-Base58.ps1

--- Encode-Base58.ps1
+++ Encode-Base58.ps1
@@ -2,13 +2,19 @@ function Encode-Base58
 {
     [cmdletbinding()]
     Param(
-        [string]$string_to_encode
+        [string]$string_to_encode,
+        [switch]$string_is_hexidecimal
     )
     $BASE58 = [System.Text.Encoding]::ASCII.GetBytes('123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz')
-    $binary_to_encode = [byte[]]::New($string_to_encode.length / 2)
-    for($i = 0; $i -lt $string_to_encode.length; $i+=2)
+    if($string_is_hexidecimal)
     {
-        $binary_to_encode[$i/2] = [Convert]::ToByte($string_to_encode.SubString($i,2),16)
+        $binary_to_encode = [byte[]]::New($string_to_encode.length / 2)
+        for($i = 0; $i -lt $string_to_encode.length; $i+=2)
+        {
+            $binary_to_encode[$i/2] = [Convert]::ToByte($string_to_encode.SubString($i,2),16)
+        }
+    } else {
+        $binary_to_encode = [System.Text.Encoding]::ASCII.GetBytes($string_to_encode)
     }
     $b58_size = 2*($binary_to_encode.length)
     $encoded = [byte[]]::New($b58_size)
